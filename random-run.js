0de8ad7d84496e4776c556dcc31db0992dcc9f71
kevinthegoose <kevin.christiansen87@gmail.com>
2023-03-30 12:50:25 -0400

    Update random-run.js

--- random-run.js
+++ random-run.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     var runLength = randomRow[headers.indexOf("Run Length")];
     
     // Create the text to display
-    var text = "Dan died on " + date + " on Run # " + runNumber + " on Floor # " + floorNumber + " because of the Piece of Piece " + deathCause + ". Dan was using the " + skin + " Skin and the run took " + runLength + " before Dan met his final demise";
+    var text = "Dan died on <span class='highlight'>" + date + "</span> on Run # <span class='highlight'>" + runNumber + "</span> on Floor # <span class='highlight'>" + floorNumber + "</span> because of the Piece of Piece <span class='highlight'>" + deathCause + "</span>. Dan was using the <span class='highlight'>" + skin + "</span> Skin and the run took <span class='highlight'>" + runLength + "</span> before Dan met his final demise";
     
     // Append the text to the 'attempt' div
     $("#attempt").append("<p>" + text + "</p>");
